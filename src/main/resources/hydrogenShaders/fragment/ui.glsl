40199ba83fa5dc54d91c21846b589cb52ef48083
Xernas <Xernas78@gmail.com>
2025-04-06 19:23:56 +0200

    Fixed shader typo

--- src/main/resources/hydrogenShaders/fragment/ui.glsl
+++ src/main/resources/hydrogenShaders/fragment/ui.glsl
@@ -16,5 +16,5 @@ uniform vec3 baseColor;
 void main() {
     vec4 currentColor = vec4(baseColor, 1.0);
     if (useTexture) currentColor = texture(textureSampler, texCoord);
-    fragColor = vec4(textured.xyz, 1.0);
+    fragColor = vec4(currentColor.xyz, 1.0);
 }
